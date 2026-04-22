0d032e767e4f4b9780d8be260b8b328c99d72163
savio rangel souza costa <sr188bet@gmail.com>
2026-04-21 21:15:07 -0300

    ID01 - inicio

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import "../src/styles/global.css"
+
+import { Slot } from "expo-router"
+
+export default function Layout() {
+ return (
+   <>
+   <Slot />
+   </>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className=" flex-1 items-center justify-center bg-red-600">
+      <Text className="">Ho111me</Text>
+    </View>
+  );
+}
